19be1985b2a4bef4ce0d88594dd295cf9dedf380
zzy012 <109944754+zzyzzy012@users.noreply.github.com>
2025-07-19 12:11:20 +0800

    配置base和deploy

--- docs/.vitepress/config.mjs
+++ docs/.vitepress/config.mjs
@@ -2,6 +2,7 @@ import { defineConfig } from 'vitepress'
 
 // https://vitepress.dev/reference/site-config
 export default defineConfig({
+  base: '/repo/',
   title: "ZZ_FT_Site",
   description: "My front-end learning journey.",
   themeConfig: {
